387140a7ff1104e5baa9c9b7760d3043ac80d3dd
Pedro <pedrovargastannuri@gmail.com>
2023-04-21 10:27:04 -0300

    ++ Chamada de funcao

--- Diqmain.hs
+++ Diqmain.hs
@@ -41,14 +41,80 @@ lingDef =
             "TInt", "TDouble", "TString", "TVoid"
           ]
     }
+
+lexico = T.makeTokenParser lingDef
+
+intOrDouble = T.naturalOrFloat lexico
+
+symbol = T.symbol lexico
+
+parens = T.parens lexico
+
+reservedOp = T.reservedOp lexico
+
+identifier = T.identifier lexico
+
+literalString = T.stringLiteral lexico
+
+comment = T.whiteSpace lexico
+
+
+-- *********************************************************************
+-- Expr
+-- *********************************************************************
+
+
+binario name fun = Infix (do {reservedOp name; return fun })
+
+prefix   name fun       = Prefix (do {reservedOp name; return fun })
+
+
 tabela   = [[prefix "-" Neg]
             , [binario "*" (:*:) AssocLeft, binario "/" (:/:) AssocLeft ]
             , [binario "+" (:+:) AssocLeft, binario "-" (:-:)   AssocLeft ]
            ]
 
 
-binario name fun = Infix (do {reservedOp name; return fun })
-prefix   name fun       = Prefix (do {reservedOp name; return fun })
+constT = try (do {n <- intOrDouble; case n of
+                                  Left num -> return (Const (CInt num))
+                                  Right num -> return (Const (CDouble num)) })
+         <|> try (do {lit <- literalString; return (Lit lit)})
+         <|> try (do {id <- identifier; exs <- expr; return (Chamada id [exs])})
+         <|> do {id <- identifier; return (IdVar id)}
+
+
+fator = parens expr
+       <|> do {c <- constT; return c}
+       <?> "simple expression"
+
+
+expr = buildExpressionParser tabela fator
+       <?> "expression" 
+  
+
+-- *********************************************************************
+-- ExprR
+-- *********************************************************************
+
+
+-- Note that we don't need to make a table, nor use buildExpressionParser for ExprR,
+-- because all operators have the same precedence level
+
+op =
+  do reservedOp "=="; return (:==:)
+    <|> do reservedOp ">="; return (:>=:)
+    <|> do reservedOp "<="; return (:<=:)
+    <|> do reservedOp ">"; return (:>:)
+    <|> do reservedOp "<"; return (:<:)
+    <|> do reservedOp "/="; return (:/=:)
+
+
+exprR =  do {e1 <- expr; o <- op; e2 <- expr; return (o e1 e2)}
+
+
+-- *********************************************************************
+-- ExprL
+-- *********************************************************************
 
 
 tabelaL = [[prefix "~" Not]
@@ -58,51 +124,20 @@ tabelaL = [[prefix "~" Not]
 
 
 binarioL name fun = Infix (do {reservedOp name; return fun })
+
 prefixL   name fun       = Prefix (do {reservedOp name; return fun })
 
-{-para o operador relacional nao precisamos fazer tabela nem o fator, pois cada operador tem a mesma precedencia-}
-op =
-  do reservedOp "=="; return (:==:)
-    <|> do reservedOp ">="; return (:>=:)
-    <|> do reservedOp "<="; return (:<=:)
-    <|> do reservedOp ">"; return (:>:)
-    <|> do reservedOp "<"; return (:<:)
-    <|> do reservedOp "/="; return (:/=:)
 
-lexico = T.makeTokenParser lingDef
-intOrDouble = T.naturalOrFloat lexico
-symbol = T.symbol lexico
-parens = T.parens lexico
-reservedOp = T.reservedOp lexico
-varIdentifier = T.identifier lexico
-literalString = T.stringLiteral lexico
-comment = T.whiteSpace lexico
-
-
-constT = do {n <- intOrDouble; case n of
-                                  Left num -> return (Const (CInt num))
-                                  Right num -> return (Const (CDouble num)) }
-         <|> do {lit <- literalString; return (Lit lit)}
-         <|> do {id <- varIdentifier; return (IdVar id)}
-{-
-Note que fator agora chama somente constT que tratara tanto de Int e Double quanto de Literais Strings
--}
-fator = parens expr
-       <|> do {c <- constT; return c}
-       <?> "simple expression"
-  
 fatorL = parens exprL
         <|> do {e <- exprR; return (Rel e)}
 
 
-expr = buildExpressionParser tabela fator
-       <?> "expression" 
-
-
-exprR =  do {e1 <- expr; o <- op; e2 <- expr; return (o e1 e2)}
-           
-
 exprL = buildExpressionParser tabelaL fatorL
+
+
+-- *********************************************************************
+-- Parser Start
+-- *********************************************************************
 
 
 partida = do comment ; try (do {e <- exprL; eof; return (show e)})
@@ -111,11 +146,12 @@ partida = do comment ; try (do {e <- exprL; eof; return (show e)})
 
 parserE  = do runParser partida [] "Expressoes"   
 
+
 parserExpr s = case parserE s of
                      Left er -> print er
                      Right v -> print v
 
+
 main = do 
-          putStr "Expressao:"
           e <- readFile "arq.txt"
           parserExpr e
--- Diqxpressions.hs
+++ Diqxpressions.hs
@@ -1,8 +1,13 @@
 module Diqxpressions where
 import Diqtypes
+
 data Expr = Expr :+: Expr | Expr :-: Expr | Expr :*: Expr | Expr :/: Expr | Neg Expr | Const TCons | IdVar String | Chamada Id [Expr] | Lit String
     deriving Show
+
+
 data ExprR = Expr :==: Expr | Expr :/=: Expr | Expr :<: Expr | Expr :>: Expr | Expr :<=: Expr | Expr :>=: Expr 
     deriving Show
+
+
 data ExprL = ExprL :&: ExprL | ExprL :|: ExprL | Not ExprL | Rel ExprR
     deriving Show
