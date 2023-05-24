48732cd0b75a4926936ed61d98a8c61e52f59f97
Pedro <pedrovargastannuri@gmail.com>
2023-05-24 19:57:48 -0300

    serase

--- DiqMonad.hs
+++ DiqMonad.hs
@@ -1,3 +1,4 @@
+module Diqmonad where
 data ErrorM a = MS (String, a)
     deriving Show
 
@@ -5,12 +6,12 @@ instance Functor ErrorM where
     fmap f (MS (s,a)) = MS(s,f a)
 
 instance Applicative ErrorM where
+    pure :: a -> ErrorM a
     pure x = MS ("",x)
     MS(s1,f) <*> MS (s2,x) = MS(s1<>s2,f x)
 
 instance Monad ErrorM where
-    return x = MS ("", x)
     MS (s,a) >>= f = let MS (s',b) = f a in MS (s++s',b)
 
-erro s = MS ("Erro:"++s,())
-adv s = MS("Adv:"++s,())
+erro s a= MS ("Erro:"++s,a)
+adv s a = MS("Adv:"++s,a)
--- Diqsem.hs
+++ Diqsem.hs
@@ -2,11 +2,28 @@ import Diqparser
 import Diqcode
 import Diqtypes
 import Diqxpressions
+import Diqmonad
 -- verProg (Prog tf fs tv b) = 
 --     do
-verExpr tab (Const(CInt i)) = return (TInt,(Const (CInt i)))
-verExpr tab (Const (CDouble d)) = return (TDouble,(Const (CDouble d)))
-verExpr tab (IdVar s) =
+verExpr tab (Const(CInt i)) = (TInt,(Const (CInt i)))
+
+verExpr tab (Const (CDouble d)) = (TDouble,(Const (CDouble d)))
+
+verExpr tab (Lit s) = (TString,(Lit s))
+
+--verExpr [] (IdVar s) = erro "Variavel nao encontrada no escopo"
+verExpr ((id :#: t):xs) (IdVar s)
+    |s == id = (t, (IdVar s))
+    |otherwise = verExpr xs (IdVar s)
+-- verExpr tab (MS ("",(Const(CInt i)))) = MS("",(TInt,(Const (CInt i))))
+-- verExpr tab (MS("",(Const (CDouble d)))) = MS("",(TDouble,(Const (CDouble d))))
+
+-- verExpr tab (MS("",(Lit s))) = MS("",(TString,(Lit s)))
+
+-- verExpr [] (MS("",(IdVar s))) = erro "Variavel nao encontrada" (TInt,IdVar s)
+-- verExpr ((id :#: t):xs) (MS("",(IdVar s)))
+--     |s == id = MS("",(t, (IdVar s)))
+--     |otherwise = verExpr xs (MS("",(IdVar s)))
  
 --verExpr tab (e1 :+: e2) =
 --    do
