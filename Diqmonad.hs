d13ca415a2438ab6893a6fe311ea332f4f14a380
Pedro <pedrovargastannuri@gmail.com>
2023-07-11 13:35:16 -0300

    less obscure monad definition

--- Diqmonad.hs
+++ Diqmonad.hs
@@ -1,6 +1,12 @@
 module Diqmonad where
 data ErrorM a = MS (String, a)
     deriving Show
+    
+erro s a= MS ("Erro:"++s,a)
+adv s a = MS("Adv:"++s,a)
+printMS (MS(s,a)) = putStrLn s >> print a
+getErro (MS(s,a)) = s
+getDado (MS(s,a)) = a
 
 instance Functor ErrorM where
     fmap f (MS (s,a)) = MS(s,f a)
@@ -10,8 +16,4 @@ instance Applicative ErrorM where
     MS(s1,f) <*> MS (s2,x) = MS(s1<>s2,f x) -- <> is a Semigroup operator. In the case of Strings, <> acts as ++ 
 
 instance Monad ErrorM where
-    MS(s,a) >>= f = let MS (s',b) = f a in MS (s++s',b)
-
-erro s a= MS ("Erro:"++s,a)
-adv s a = MS("Adv:"++s,a)
-printMS (MS(s,a)) = putStrLn s >> print a
+    MS(s,a) >>= f = MS (s++(getErro (f a)),getDado(f a))
